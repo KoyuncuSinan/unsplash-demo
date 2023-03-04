3aadbe2b07484be3db74a37dfaf91982cce050b4
Sinan Koyuncu <koyuncusinan1@gmail.com>
2023-03-04 12:50:33 +0300

    Homepage turn into masonry style

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,15 +1,16 @@
 import React, {useState,useEffect} from "react";
 import { useNavigate } from "react-router-dom";
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
-import ImageListItemBar from '@mui/material/ImageListItemBar';
-import ListSubheader from '@mui/material/ListSubheader';
-import IconButton from '@mui/material/IconButton';
-import InfoIcon from '@mui/icons-material/Info';
+import Masonry from "react-masonry-css";
 
 export default function Homepage(){
     const [images, setImages] = useState([])
     const navigate = useNavigate();
+    const breakpointColumnsObj = {
+        default: 3,
+        1100: 3,
+        700: 2,
+        500: 1,
+    }
 
     useEffect(() => {
         const getImages = async () => {
@@ -44,32 +45,17 @@ export default function Homepage(){
     //
 
     return(
-        <ImageList sx={{width:500,height:450}}>
-            <ImageListItem key="Subheader" cols={2}>
-                <ListSubheader component="div">Gallery</ListSubheader>
-            </ImageListItem>
-            {images.map((item) => (
-                <ImageListItem key={item._id}>
-                    <img 
-                        src={`${item.imagePath}?w=248&fit=crop&auto=format`}
-                        srcSet={`${item.imagePath}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                        alt={item.label}
-                        loading= "lazy"
-                    />
-                    <ImageListItemBar
-                        title={item.label}
-                        actionIcon={
-                            <IconButton
-                                sx={{color: "rgba(255, 255, 255, 0.54)"}}
-                                aria-label={`info about ${item.label}`}
-                            >
-                                <InfoIcon/>
-                            </IconButton>
-                        }
-                       /> 
-                </ImageListItem>
-            ))};
-        </ImageList>
+        <Masonry
+            breakpointCols={breakpointColumnsObj}
+            className="my-masonry-grid"
+            columnClassName="my-masonry-grid_column">
+            {images.length >= 1 ? (images.map((image,index) => (
+                <div key={image._id} className= "image"  >
+                    <img src = {image.imagePath}  className="homeImage" onClick={() => navigate(`/${image._id}`)}></img>
+                    <h2 className="label">{image.label}</h2>
+                </div>
+            ))): "Loading"}
+        </Masonry>
     )
 
 }
