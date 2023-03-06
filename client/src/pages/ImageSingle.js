4d8b071114b41741a6655a02c43c67aa1ab56832
Sinan Koyuncu <koyuncusinan1@gmail.com>
2023-03-06 16:37:34 +0300

    Error page for non-exist image

--- client/src/pages/ImageSingle.js
+++ client/src/pages/ImageSingle.js
@@ -1,27 +1,27 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import jwt_decode from "jwt-decode";
-import DeleteIcon from '@mui/icons-material/Delete';
+import DeleteIcon from "@mui/icons-material/Delete";
+import errorImage from "../style/detective-dog.jpg";
 
 export default function ImageSingle() {
   const [imagePage, setImagePage] = useState("");
   const [isLogged, setIsLogged] = useState(false);
   const [errorMessage, setErrorMessage] = useState(null);
-  const [isLoading, setIsLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState(true);
   const navigate = useNavigate();
   const { id } = useParams();
 
   const token = localStorage.getItem("token");
-  let userId = null
-  if (token){
+  let userId = null;
+  if (token) {
     userId = jwt_decode(token).id;
-
   }
-  
+
   useEffect(() => {
     if (token) {
-        setIsLogged(true);
-      }
+      setIsLogged(true);
+    }
     const getImage = async () => {
       try {
         const res = await fetch(`http://localhost:3001/${id}`, {
@@ -33,17 +33,16 @@ export default function ImageSingle() {
         if (!res.ok) {
           const errorData = await res.json();
           setErrorMessage(errorData.msg);
-        } else{
+        } else {
           const data = await res.json();
           if (data) {
             console.log(data);
             setImagePage(data);
           }
-
         }
       } catch (err) {
         console.log(err);
-        setErrorMessage("An error occurred while fetching the image")
+        setErrorMessage("An error occurred while fetching the image");
       } finally {
         setIsLoading(false);
       }
@@ -55,8 +54,8 @@ export default function ImageSingle() {
     try {
       const res = await fetch(`http://localhost:3001/${id}`, {
         method: "DELETE",
-        headers:{
-            Authorization: `Bearer ${token}`
+        headers: {
+          Authorization: `Bearer ${token}`,
         },
       });
       if (res.ok) {
@@ -69,30 +68,31 @@ export default function ImageSingle() {
     }
   };
 
-  if(isLoading){
-    return <div >Loading...</div>
+  if (isLoading) {
+    return <div>Loading...</div>;
   }
 
   return (
     <main id="singlePage">
-      
       {imagePage.length !== 0 ? (
-        <div key={imagePage._id} id= "single-container">
+        <div key={imagePage._id} id="single-container">
           <h2 className="pageLabel">{imagePage.label}</h2>
           <div className="icon-container">
-          <img src={imagePage.imagePath} className="singleImage"></img>
+            <img src={imagePage.imagePath} className="singleImage"></img>
 
             {token && imagePage.owner === userId && (
-                <DeleteIcon onClick={deleteImage} className="deleteIcon"/>  
-
-              
+              <DeleteIcon onClick={deleteImage} className="deleteIcon" />
             )}
-
           </div>
         </div>
-      ) : <h2 className="error-message">Oops! The image you're looking for doesn't exist.</h2>
-        
-      }
+      ) : (
+        <div className="image-error">
+          <h2 className="error-message">
+            Oops! The image you're looking for doesn't exist.
+          </h2>
+          <img src={errorImage} alt="Error Image" className="error-dog"></img>
+        </div>
+      )}
     </main>
   );
 }
